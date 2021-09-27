00e2cdb291390996dfb4a782b15d0af8e111953a
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-09-27 06:52:04 -0300

    :ballot_box_with_check: challenge completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,6 @@
+import { memo } from "react";
+import { AutoSizer, List, ListRowRenderer } from "react-virtualized";
+
 import { Button } from "./Button";
 
 interface SideBarProps {
@@ -10,27 +13,43 @@ interface SideBarProps {
   buttonClickCallback: (args: any) => void;
 }
 
-export function SideBar({
+function PageSideBar({
   genres,
   selectedGenreId,
   buttonClickCallback
 }: SideBarProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={ key } style={ style }>
+        <Button
+          title={ genres[index].title }
+          iconName={ genres[index].name }
+          onClick={ () => buttonClickCallback(genres[index].id) }
+          selected={ selectedGenreId === genres[index].id }
+        />
+      </div>
+    )
+  }
+
   return (
     <nav className="sidebar">
       <span>Watch<p>Me</p></span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
-            selected={selectedGenreId === genre.id}
-          />
-        ))}
+        <List
+          height={ 300 }
+          rowHeight={ 80 }
+          width={ 350 }
+          overscanRowCount={ 2 }
+          rowCount={ genres.length }
+          rowRenderer={ rowRenderer }
+        />
       </div>
 
     </nav>
   )
 }
+
+export const SideBar = memo(PageSideBar, (prevProps, nextProps) => {
+  return Object.is(prevProps, nextProps)
+})
